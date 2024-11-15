927d5e453939bde0dede3243d60c53c4c75137db
liuyuhe666 <liuyuhe666@users.noreply.github.com>
2024-11-15 04:20:11 +0000

    deploy: 6c661c4b8cb1765509e25ac875209331f4c2a4a7

---
+++ lib/html.ts
@@ -0,0 +1,15 @@
+import type { VideoInfo } from './bilibili'
+import { readFile, writeFile } from 'node:fs/promises'
+import MarkdownIt from 'markdown-it'
+import { gc, m } from '../utils'
+import { generateContent } from './common'
+
+export async function generateHTML(data: VideoInfo[]) {
+  const md = new MarkdownIt({
+    html: true,
+  })
+  const htmlTemplate = await readFile('./templates/index.template.html', { encoding: 'utf-8' })
+  const content = generateContent(data, htmlTemplate)
+  const result = m`${md.render(content)}`
+  await writeFile('./index.html', result, { encoding: 'utf-8' })
+}
---
+++ templates/README.template.md
@@ -0,0 +1,14 @@
+<div align="center">
+    <img src="./assets/icon_rank.png" alt="logo" />
+    <h2>哔哩哔哩排行榜</h>
+</div>
+
+> 排行榜根据稿件内容质量，近期的数据综合展示，动态更新
+
+<br />
+
+<!-- content_inject -->
+
+<br />
+
+<!-- footer_inject -->
